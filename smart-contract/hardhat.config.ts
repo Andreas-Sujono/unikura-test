27699934fa45e13e077fce2b3c8df60943e62c3b
andreas sujono <andr0075@e.ntu.edu.sg>
2024-02-11 15:56:13 +0800

    init unit test for XShoe NFT

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -2,7 +2,23 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
+  networks: {
+    // BSC Testnet
+    bscTestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545", // BSC Testnet endpoint
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "", // Your BSC Testnet mnemonic
+      },
+    },
+    // Mumbai Testnet
+    mumbaiTestnet: {
+      url: "https://matic-mumbai.chainstacklabs.com", // Mumbai Testnet endpoint
+      accounts: {
+        mnemonic: process.env.MNEMONIC || "", // Your Mumbai Testnet mnemonic
+      },
+    },
+  },
 };
 
 export default config;
---
+++ smart-contract/test/XShoeNFT.test.ts
@@ -0,0 +1,53 @@
+import { ethers } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+
+describe("XShoeNFT", function () {
+  const NFTName = "MyNFT";
+  const NFTSymbol = "NFT";
+  const NFTBaseURI =
+    "https://amber-particular-cuckoo-729.mypinata.cloud/ipfs/QmTctBgr9NsQ1qKkU8yCidke6bAzN3WqLBXqyL1KMgp8J3";
+
+  async function deployNFTFixture() {
+    const [owner, addr1] = await ethers.getSigners();
+
+    const XShoeNFT = await ethers.getContractFactory("XShoeNFT");
+    const xShoeNFT = await XShoeNFT.deploy(NFTName, NFTSymbol, NFTBaseURI);
+
+    return { xShoeNFT, owner, addr1 };
+  }
+
+  it("Should set the correct NFT base information", async function () {
+    const { xShoeNFT } = await loadFixture(deployNFTFixture);
+
+    expect(await xShoeNFT.symbol()).to.equal(NFTSymbol);
+    expect(await xShoeNFT.name()).to.equal(NFTName);
+    expect(await xShoeNFT["tokenURI()"]()).to.equal(NFTBaseURI);
+  });
+
+  it("Should mint a token", async function () {
+    const { xShoeNFT, addr1 } = await loadFixture(deployNFTFixture);
+
+    await xShoeNFT.connect(addr1).mint(addr1.address);
+    expect(await xShoeNFT.ownerOf(1)).to.equal(addr1.address);
+  });
+
+  it("Should emit TokenMinted event when minting a token", async function () {
+    const { xShoeNFT, addr1 } = await loadFixture(deployNFTFixture);
+
+    await expect(xShoeNFT.connect(addr1).mint(addr1.address))
+      .to.emit(xShoeNFT, "TokenMinted")
+      .withArgs(addr1.address, 1);
+  });
+
+  it("Should emit TokenURIChanged event when updating token URI", async function () {
+    const { xShoeNFT, owner } = await loadFixture(deployNFTFixture);
+
+    const newTokenURI = "https://newMetadata.com/";
+    await expect(xShoeNFT.connect(owner).setTokenURI(newTokenURI))
+      .to.emit(xShoeNFT, "TokenURIChanged")
+      .withArgs(newTokenURI);
+
+    expect(await xShoeNFT["tokenURI()"]()).to.equal(newTokenURI);
+  });
+});
